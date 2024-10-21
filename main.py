38494b319ef4737a3304a2921e8287d23cd327fa
hugopavy <hugo.pavy@etu.minesparis.psl.eu>
2024-10-21 09:04:36 +0000

    ok relance

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 # Avec Z et le choix des points avec une certaine proba
 from deepxrte.geometry import Rectangle
-import torch 
-import torch.nn as nn 
+import torch
+import torch.nn as nn
 import torch.optim as optim
-from matplotlib.animation import FuncAnimation
 from model import PINNs
 from utils import read_csv, write_csv
 from train import train
 from pathlib import Path
-import time 
+import time
 import pandas as pd
 import numpy as np
 
@@ -19,33 +18,35 @@ time_start = time.time()
 
 ############# LES VARIABLES ################
 
-folder_result = '4_new_interval'  # le nom du dossier de résultat
+folder_result = '1_first_try'  # le nom du dossier de résultat
+
+random_seed_train = 2632  # la seed de train, la changer pour avoir de nouveau résultats
+random_seed_test = 2002   # la seed de test, toujours garder la même pour pouvoir comparer
 
 
-torch.manual_seed(42537)
 
 ##### Le modèle de résolution de l'équation de la chaleur
-nb_itt = 5     # le nb d'epoch
-resample_rate = 3000  # le taux de resampling
+nb_itt = 10000     # le nb d'epoch
+resample_rate = 300000  # le taux de resampling
 display = 500       # le taux d'affichage
 poids = [1, 1]   # les poids pour la loss
-    
-n_data = 5000         # le nb de points initiaux
-n_pde = 5000          # le nb de points pour la pde
+
+n_data = 10000         # le nb de points initiaux
+n_pde = 10000          # le nb de points pour la pde
 
 n_data_test = 5000
-n_pde_test  = 5000
+n_pde_test = 5000
 
 Re = 100
 
-lr = 1e-4
+lr = 5e-4
 
 x_proba_max = 0.3
 x_proba_min = 0.03
 y_proba_max = 0.15
 y_proba_min = 0.05
 
-proba = 0.8
+proba = 0.5
 
 ##### Le code ###############################
 ###############################################
@@ -55,9 +56,11 @@ df = pd.read_csv('data.csv')
 
 
 # On adimensionne la data
-df_modified = df[(df['Points:0']>= -0.07) & (df['Points:1']>= -0.1) & (df['Points:1']<= 0.1)]
-x, y, t = np.array(df_modified['Points:0']), np.array(df_modified['Points:1']), np.array(df_modified['Time'])
-u, v, p = np.array(df_modified['Velocity:0']), np.array(df_modified['Velocity:1']), np.array(df_modified['Pressure'])
+df_modified = df[(df['Points:0'] >= -0.07) & (df['Points:1'] >= -0.1) & (df['Points:1'] <= 0.1)]
+x, y, t = (np.array(df_modified['Points:0']), np.array(df_modified['Points:1']),
+           np.array(df_modified['Time']))
+u, v, p = (np.array(df_modified['Velocity:0']), np.array(df_modified['Velocity:1']),
+           np.array(df_modified['Pressure']))
 x = x-x.min()
 y = y-y.min()
 
@@ -86,28 +89,34 @@ x_norm_max = x_norm.max()
 y_norm_max = y_norm.max()
 
 
-# On regarde si le dossier existe 
+# On regarde si le dossier existe
 dossier = Path(folder_result)
 dossier.mkdir(parents=True, exist_ok=True)
 
-rectangle_proba = Rectangle(x_max = x_proba_norm_max, y_max = y_proba_norm_max,
-                      t_min=t_norm_min, t_max=t_norm_max)    # le domaine de plus haute proba 
+torch.manual_seed(random_seed_train)
 
-rectangle = Rectangle(x_max = x_norm_max, y_max = y_norm_max,
+rectangle_proba = Rectangle(x_max=x_proba_norm_max, y_max=y_proba_norm_max,
+                            t_min=t_norm_min, t_max=t_norm_max)    # le domaine de plus haute proba
+
+rectangle = Rectangle(x_max=x_norm_max, y_max=y_norm_max,
                       t_min=t_norm_min, t_max=t_norm_max)    # le domaine de résolution
 
 
-# les points initiaux du train 
-# Les points de pde 
+# les points initiaux du train
+# Les points de pde
 
 
 ### Pour train
-points_pde_proba = rectangle_proba.generate_random(int(n_pde*proba)).to(device)   # les points pour la pde
-points_pde_non_proba = rectangle.generate_random(n_pde-int(n_pde*proba)).to(device)   # les points pour la pde
+# les points pour la pde
+points_pde_proba = rectangle_proba.generate_random(int(n_pde*proba)).to(device)
+points_pde_non_proba = rectangle.generate_random(n_pde-int(n_pde*proba)).to(device)
 points_pde = torch.cat((points_pde_proba, points_pde_non_proba), 0)
 
+
 # On prend des points de data uniquement dans notre rectangle de proba
-masque = (X[:,0]> x_proba_min)&(X[:,0]< x_proba_max)&(X[:,1]> y_proba_min)&(X[:,1]< y_proba_max)
+np.random.seed(random_seed_train)
+masque = ((X[:, 0] > x_proba_min) & (X[:, 0] < x_proba_max) &
+          (X[:, 1] > y_proba_min) & (X[:, 1] < y_proba_max))
 points_data_train_proba = np.random.choice(len(X[masque]), int(n_data*proba), replace=False)
 inputs_train_data_proba = torch.from_numpy(X[masque][points_data_train_proba]).requires_grad_().to(device)
 outputs_train_data_proba = torch.from_numpy(U[masque][points_data_train_proba]).requires_grad_().to(device)
@@ -117,11 +126,16 @@ outputs_train_data_non_proba = torch.from_numpy(U[points_data_train_non_proba]).
 inputs_train_data = torch.cat((inputs_train_data_proba, inputs_train_data_non_proba), 0)
 outputs_train_data = torch.cat((outputs_train_data_proba, outputs_train_data_non_proba), 0)
 
+
+
 ### Pour test
+torch.manual_seed(random_seed_test)
+np.random.seed(random_seed_test)
 X_test_pde = rectangle.generate_random(n_pde_test).to(device)
 points_coloc_test = np.random.choice(len(X), n_data_test, replace=False)
 X_test_data = torch.from_numpy(X[points_coloc_test]).requires_grad_().to(device)
 U_test_data = torch.from_numpy(U[points_coloc_test]).requires_grad_().to(device)
+
 
 
 # Initialiser le modèle
@@ -132,23 +146,26 @@ loss = nn.MSELoss()
 # On plot les print dans un fichier texte 
 with open(folder_result+'/print.txt', 'a') as f:
     # On regarde si notre modèle n'existe pas déjà 
-    if Path(folder_result+'/model_weights.pth').exists() :
-        model.load_state_dict(torch.load(folder_result+'/model_weights.pth'))
+    if Path(folder_result+'/model_weights.pth').exists():
+        # Charger l'état du modèle et de l'optimiseur
+        checkpoint = torch.load(folder_result+'/model_weights.pth')
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         print("\nModèle chargé\n", file=f)
         print("\nModèle chargé\n")
         train_loss = read_csv(folder_result+'/train_loss.csv')['0'].to_list()
         test_loss = read_csv(folder_result+'/test_loss.csv')['0'].to_list()
         print("\nLoss chargée\n", file=f)
         print("\nLoss chargée\n")
-        
-    else : 
+
+    else:
         print('Nouveau modèle\n', file=f)
         print('Nouveau modèle\n')
         train_loss = []
         test_loss = []
 
 
-    ######## On entraine le modèle 
+    ######## On entraine le modèle
     ###############################################
     train(nb_itt=nb_itt, train_loss=train_loss, test_loss=test_loss, resample_rate=resample_rate,
           display=display, poids=poids, inputs_train_data=inputs_train_data,
@@ -156,12 +173,14 @@ with open(folder_result+'/print.txt', 'a') as f:
           model=model, loss=loss, optimizer=optimizer, X=X, U=U, n_pde=n_pde, X_test_pde=X_test_pde,
           X_test_data=X_test_data, U_test_data=U_test_data, n_data=n_data, rectangle=rectangle,
           device=device, Re=Re, time_start=time_start, f=f,
-          u_mean = u.mean(), v_mean = v.mean(), x_std = x.std(), y_std = y.std(),
-          t_std = t.std(), u_std = u.std(), v_std = v.std(), p_std = p.std(), masque=masque,
+          u_mean=u.mean(), v_mean=v.mean(), x_std=x.std(), y_std=y.std(),
+          t_std=t.std(), u_std=u.std(), v_std=v.std(), p_std=p.std(), masque=masque,
           proba=proba, rectangle_proba=rectangle_proba)
 
-    ####### On save le model et les losses
-    torch.save(model.state_dict(), folder_result+'/model_weights.pth')
-    write_csv(train_loss, folder_result, file_name='/train_loss.csv')
-    write_csv(test_loss, folder_result, file_name='/test_loss.csv')
-    
+####### On save le model et les losses
+torch.save({
+    'model_state_dict': model.state_dict(),
+    'optimizer_state_dict': optimizer.state_dict(),
+}, folder_result+'/model_weights.pth')
+write_csv(train_loss, folder_result, file_name='/train_loss.csv')
+write_csv(test_loss, folder_result, file_name='/test_loss.csv')
--- train.py
+++ train.py
@@ -9,49 +9,53 @@ def train(nb_itt, train_loss, test_loss, resample_rate, display, poids,
           model, loss, optimizer, X, U, n_pde, X_test_pde,
           X_test_data, U_test_data, n_data, rectangle, device, Re, 
           time_start, f, x_std, y_std, u_mean, v_mean, p_std, t_std, u_std, v_std, masque, proba, 
-          rectangle_proba): 
+          rectangle_proba):
     nb_it_tot = nb_itt + len(train_loss)
     # Nos datas initiales
     X_train_data = inputs_train_data
     U_train_data = outputs_train_data
     X_train_pde = points_pde
-    print(f'--------------------------\nStarting at epoch: {len(train_loss)}\n--------------------------')
-    print(f'--------------------------\nStarting at epoch: {len(train_loss)}\n--------------------------', file=f)
+    print(f'--------------------------\nStarting at epoch: {len(train_loss)}' +
+          '\n--------------------------')
+    print(f'--------------------------\nStarting at epoch: {len(train_loss)}\n------------' +
+          '--------------', file=f)
     
-    for epoch in range(len(train_loss), nb_it_tot) : 
-        model.train() # on dit qu'on va entrainer (on a le dropout)
+    for epoch in range(len(train_loss), nb_it_tot): 
+        model.train()  # on dit qu'on va entrainer (on a le dropout)
 
         ## loss du pde
         pred_pde = model(X_train_pde)
-        pred_pde1, pred_pde2, pred_pde3 = pde(pred_pde, X_train_pde, Re=Re, 
-                                              x_std=x_std, y_std=y_std, u_mean=u_mean, v_mean=v_mean,
-                                              p_std=p_std, t_std=t_std, u_std=u_std, v_std=v_std) 
-        loss_pde = torch.mean(torch.abs(pred_pde1)) + torch.mean(torch.abs(pred_pde2)) + torch.mean(torch.abs((pred_pde3))) #(MSE) 
-        
+        pred_pde1, pred_pde2, pred_pde3 = pde(pred_pde, X_train_pde, Re=Re,
+                                              x_std=x_std, y_std=y_std, u_mean=u_mean,
+                                              v_mean=v_mean,
+                                              p_std=p_std, t_std=t_std, u_std=u_std, v_std=v_std)
+        loss_pde = (torch.mean(torch.abs(pred_pde1)) + 
+                    torch.mean(torch.abs(pred_pde2)) + torch.mean(torch.abs((pred_pde3))))   # (MSE)
+
 
         # loss des points de data
         pred_data = model(X_train_data)
-        loss_data = loss(U_train_data, pred_data)  #(MSE)
+        loss_data = loss(U_train_data, pred_data)  # (MSE)
 
         # loss totale
         loss_totale = poids[0]*loss_data + poids[1]*loss_pde
         train_loss.append(loss_totale.item())
-        
+
         # Backpropagation
         loss_totale.backward(retain_graph=True)
         optimizer.step()
         optimizer.zero_grad()
-        
+
         # Pour le test :
         model.eval()
-        
+
         # loss du pde
         test_pde = model(X_test_pde)
         test_pde1, test_pde2, test_pde3 = pde(test_pde, X_test_pde, Re=Re,
                                               x_std=x_std, y_std=y_std, u_mean=u_mean, v_mean=v_mean,
                                               p_std=p_std, t_std=t_std, u_std=u_std, v_std=v_std) 
         loss_test_pde = torch.mean(torch.abs(test_pde1)) + torch.mean(torch.abs(test_pde2)) + torch.mean(torch.abs((test_pde3))) #(MSE) 
-        
+
         # loss de la data
         test_data = model(X_test_data)
         loss_test_data = loss(U_test_data, test_data) #(MSE)
@@ -69,15 +73,17 @@ def train(nb_itt, train_loss, test_loss, resample_rate, display, poids,
             print(f"Test  : loss: {np.mean(test_loss[-display:]):.3e}, data: {loss_test_data:.3e}, pde: {loss_test_pde:.3e}", file=f)
             print(f"time: {time.time()-time_start:.0f}s")
             print(f"time: {time.time()-time_start:.0f}s", file=f)
-            
-        if  (epoch <= 4) :
-            print(f"Epoch: {epoch+1}/{nb_it_tot}, loss: {train_loss[-1]:.3e}, data: {loss_data:.3e}, pde: {loss_pde:.3e}")
-            print(f"Epoch: {epoch+1}/{nb_it_tot}, loss: {train_loss[-1]:.3e}, data: {loss_data:.3e}, pde: {loss_pde:.3e}", file=f)
-        
+
+        if (epoch <= 4):
+            print(f"Epoch: {epoch+1}/{nb_it_tot}, loss: {train_loss[-1]:.3e}," +
+                   " data: {loss_data:.3e}, pde: {loss_pde:.3e}")
+            print(f"Epoch: {epoch+1}/{nb_it_tot}, loss: {train_loss[-1]:.3e}," +
+                  " data: {loss_data:.3e}, pde: {loss_pde:.3e}", file=f)
+
         if (epoch+1) % resample_rate == 0:
             # On resample les points
-            
-            # Data 
+
+            # Data
             points_data_train_proba = np.random.choice(len(X[masque]), int(n_data*proba), replace=False)
             inputs_train_data_proba = torch.from_numpy(X[masque][points_data_train_proba]).requires_grad_().to(device)
             outputs_train_data_proba = torch.from_numpy(U[masque][points_data_train_proba]).requires_grad_().to(device)
